ce3ad57f28ac2fc7d7b929ccf72f3a19fd50a7d4
Emily Norman <emily.norman@outlook.com>
2023-10-04 10:12:33 -0400

    update database to add additional user information

--- app/models/user.py
+++ app/models/user.py
@@ -13,6 +13,12 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
+    city = db.Column(db.String(40), nullable=False)
+    state = db.Column(db.String(40), nullable=False)
+    country = db.Column(db.String(40), nullable=False)
+    bio = db.Column(db.String(255))
 
     @property
     def password(self):
@@ -29,5 +35,11 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'city': self.city,
+            'state': self.state,
+            'country': self.country,
+            'bio': self.bio
         }
--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -9,6 +9,12 @@ function SignupFormModal() {
 	const [email, setEmail] = useState("");
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [city, setCity] = useState('');
+	const [state, setState] = useState('');
+	const [country, setCountry] = useState('');
+	const [bio, setBio] = useState('');
 	const [confirmPassword, setConfirmPassword] = useState("");
 	const [errors, setErrors] = useState([]);
 	const { closeModal } = useModal();
@@ -16,7 +22,7 @@ function SignupFormModal() {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		if (password === confirmPassword) {
-			const data = await dispatch(signUp(username, email, password));
+			const data = await dispatch(signUp(username, email, password, firstName, lastName, city, state, country, bio));
 			if (data) {
 				setErrors(data);
 			} else {
@@ -71,6 +77,59 @@ function SignupFormModal() {
 						type="password"
 						value={confirmPassword}
 						onChange={(e) => setConfirmPassword(e.target.value)}
+						required
+					/>
+				</label>
+				<label>
+					First Name
+					<input
+						type="text"
+						value={firstName}
+						onChange={(e) => setFirstName(e.target.value)}
+						required
+					/>
+				</label>
+				<label>
+					Last Name
+					<input
+						type="text"
+						value={lastName}
+						onChange={(e) => setLastName(e.target.value)}
+						required
+					/>
+				</label>
+				<label>
+					City
+					<input
+						type="text"
+						value={city}
+						onChange={(e) => setCity(e.target.value)}
+						required
+					/>
+				</label>
+				<label>
+					State
+					<input
+						type="text"
+						value={state}
+						onChange={(e) => setState(e.target.value)}
+						required
+					/>
+				</label>
+				<label>
+					Country
+					<input
+						type="text"
+						value={country}
+						onChange={(e) => setCountry(e.target.value)}
+						required
+					/>
+				</label>
+				<label>
+					Bio
+					<textarea
+						value={bio}
+						onChange={(e) => setBio(e.target.value)}
 						required
 					/>
 				</label>
