74077256b331459cdf78ccf2d0461fadeb92b805
Emily <emily.norman@outlook.com>
2023-10-06 19:20:27 -0400

    add fronend route to upload profile picture

--- react-app/src/components/AddProfilePicturePage/index.js
+++ react-app/src/components/AddProfilePicturePage/index.js
@@ -1,21 +1,79 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useRef, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { addProfilePicture } from "../../store/session";
 import "./index.css";
-
+import default_picture from "../../images/default-profile-picture.jpg";
 
 function AddProfilePicture() {
+  const inputRef = useRef(null);
+  const dispatch = useDispatch();
   const history = useHistory();
   const sessionUser = useSelector((state) => state.session.user);
+  const [image, setImage] = useState("");
 
   console.log(sessionUser);
   if (!sessionUser) history.push("/");
+
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    const imgname = event.target.files[0].name;
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      const img = new Image();
+      img.src = reader.result;
+      img.onload = () => {
+        const canvas = document.createElement("canvas");
+        const maxSize = Math.max(img.width, img.height);
+        canvas.width = maxSize;
+        canvas.height = maxSize;
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(
+          img,
+          (maxSize - img.width) / 2,
+          (maxSize - img.height) /2
+        );
+        canvas.toBlob(
+          (blob => {
+            const file = new File([blob], imgname, {
+              type:image/ProcessingInstruction,
+              lastModified: Date.now()
+            });
+            console.log(file);
+            setImage(file);
+          }),
+          "image/jpeg",
+          0.8
+        )
+      }
+    }
+  };
+
+  const handleUpload = async () => {
+    const data = await dispatch(addProfilePicture(sessionUser.id, image))
+    if(data){
+      history.push('/')
+    }
+  }
 
   return (
     <div className="picture-page-container">
       <div className="main-content">
         <h1>Upload A Profile Photo</h1>
-
+        <div onClick={handleClick} className="user-picture-input">
+          {image ? (
+            <img src={URL.createObjectURL(image)} alt=" " className="profile-picture-image" />
+          ) : (
+            <img src={default_picture} alt=" " className="profile-picture-image" />
+          )}
+          <input type="file" ref={inputRef} onChange={handleImageChange} />
+        </div>
+        <button className="image-upload-button" onClick={handleUpload}>Upload</button>
       </div>
     </div>
   );
