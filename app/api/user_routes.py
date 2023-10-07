387b4b4a806a0efef2f818e8c59d36fd4c7f72ef
Emily <emily.norman@outlook.com>
2023-10-06 22:26:23 -0400

    add backend route to add post

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import User, db
+from app.models import User, Post, db
 from app.forms import ProfilePictureForm
+from app.forms import PostForm
+from flask_login import current_user
 import boto3
 import os
 
 
 user_routes = Blueprint('users', __name__)
-s3 = boto3.client('s3', aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.environ.get("AWS_SECRET_KEY"))
+s3 = boto3.client('s3', aws_access_key_id=os.environ.get(
+    "AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.environ.get("AWS_SECRET_KEY"))
 
 
 @user_routes.route('/')
@@ -29,6 +32,7 @@ def user(id):
     user = User.query.get(id)
     return user.to_dict()
 
+
 @user_routes.route('/<int:id>/add-profile-photo', methods=['POST'])
 @login_required
 def add_profile_picture(id):
@@ -41,11 +45,36 @@ def add_profile_picture(id):
         unique_filename = f"user_{user.id}_profile_picture.jpg"
 
         try:
-            s3.upload_fileobj(uploaded_file, 'stellar-society', unique_filename)
+            s3.upload_fileobj(
+                uploaded_file, 'stellar-society', unique_filename)
 
             user.profile_picture = f"https://stellar-society.s3.us-east-2.amazonaws.com/{unique_filename}"
             db.session.commit()
             return user.to_dict()
         except Exception as e:
-             return jsonify({"error": f"Failed to upload the profile picture to S3: {str(e)}"}), 500
+            return jsonify({"error": f"Failed to upload the profile picture to S3: {str(e)}"}), 500
     return jsonify({"error": "Failed to update the profile picture.", "errors": form.errors}), 400
+
+
+@user_routes.route('/<int:id>/post', methods=['POST'])
+@login_required
+def add_post(id):
+    if id != current_user.id:
+        return jsonify({"error": "Unauthorized"}), 403
+
+    form = PostForm()
+
+    if form.validate_on_submit():
+        try:
+            post = Post(
+                user_id=id,
+                content=form.data['content']
+            )
+            db.session.add(post)
+            db.session.commit()
+            return post.to_dict(), 201
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"error": str(e)}), 500
+    else:
+        return jsonify({"error": "Invalid form data", "errors": form.errors}), 400
